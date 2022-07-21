534a4e975aa74fd5af8756f27f6bd4419c0349b9
Souchet Paul <xiiip@hotmail.fr>
2022-07-21 12:44:01 +0200

    Add indexedDB

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from "react";
 // import PropTypes from "prop-types";
 import CardList from "../cardList";
+import { db } from "../../db";
 
 /**
  * Component
  */
 const Game = () => {
+  const initialCards = [
+    {
+      url: "https://picsum.photos/201",
+      title: "bip",
+      uuid: "51d48348-1990-4f91-8432-1e4aeeed6fc2",
+    },
+    {
+      url: "https://picsum.photos/200",
+      title: "bipBIP",
+      uuid: "a5933fa2-07fa-439d-9f47-2d98df6c7e26",
+    },
+  ];
   const [cards, setCards] = useState([]);
-  const loadFromIndexDB = () =>
-    Promise.resolve([
-      {
-        url: "https://picsum.photos/201",
-        title: "bip",
-        uuid: "51d48348-1990-4f91-8432-1e4aeeed6fc2",
-      },
-      {
-        url: "https://picsum.photos/200",
-        title: "bipBIP",
-        uuid: "a5933fa2-07fa-439d-9f47-2d98df6c7e26",
-      },
-    ]);
+
   // On mount (once), we load the cards from the store
   useEffect(() => {
-    loadFromIndexDB().then((data) => console.warn(data) || setCards(data));
+    const loadFromIndexDB = async () => {
+      let cards = await db.cards.toArray();
+      if (cards.length === 0) {
+        const cardsIds = await db.cards.bulkAdd(initialCards, {
+          allKeys: "true",
+        });
+        cards = await db.cards.bulkGet(cardsIds);
+      }
+      setCards(cards);
+    };
+    loadFromIndexDB().catch((e) => console.error(e));
+
+    return () => setCards([]);
   }, []);
 
   return (
